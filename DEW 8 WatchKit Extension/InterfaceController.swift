611ab26c84d67b516acc5dc4c3353100aa7cfd7c
rob cooper <skybolt@gmail.com>
2018-05-10 11:32:08 -0700

    Initial Commit

---
+++ DEW 8 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DEW 8 WatchKit Extension
+//
+//  Created by rob on 5/10/18.
+//  Copyright © 2018 rob. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ DEW 8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DEW 8
+//
+//  Created by rob on 5/10/18.
+//  Copyright © 2018 rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
